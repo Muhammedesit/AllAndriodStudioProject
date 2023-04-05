89ac7d95540467bd7f2db8f58906acc60dbb79c2
m.esit <82802373+Muhammedesit@users.noreply.github.com>
2023-04-05 12:42:49 +0300

    Add files via upload

---
+++ thrift_shop_v20/android/app/src/main/java/com/example/thrift_shop_v20/thrift_shop_v20/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.thrift_shop_v20.thrift_shop_v20;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ thrift_shop_v20/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'providers/cart.dart';
+import 'providers/orders.dart';
+import 'providers/products.dart';
+import 'screens/cart_screen.dart';
+import 'screens/edit_product_screen.dart';
+import 'screens/orders_screen.dart';
+import 'screens/product_detail_screen.dart';
+import 'screens/products_overview_screen.dart';
+import 'screens/user_products_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Orders(),
+        ),
+      ],
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                .copyWith(secondary: Colors.deepOrange),
+            fontFamily: 'Lato',
+          ),
+          home: const ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+            CartScreen.routeName: (ctx) => const CartScreen(),
+            OrdersScreen.routeName: (ctx) => const OrdersScreen(),
+            UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
+            EditProductScreen.routeName: (ctx) => const EditProductScreen(),
+          }),
+    );
+  }
+}
